6f033d7e2a4172cae5a4b095e2614744caea9292
jessabean <jess@eldredge.io>
2023-01-06 11:46:30 -0500

    Set up menu toggle

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,25 @@
 import './App.css';
 import Editor from '../Editor/Editor';
+import Nav from '../Nav/Nav';
 import { useState, useEffect } from 'react';
 
 function App() {
   const [theme, setTheme] = useState('light');
+  const [showMenu, setShowMenu] = useState(false);
+
+  function handleToggle(e) {
+    setShowMenu(e);
+  }
 
   useEffect(() => {
     setTheme(theme);
+    setShowMenu(showMenu);
     document.documentElement.setAttribute('data-theme', theme);
-  }, [theme]);
+  }, [theme, showMenu]);
 
   return (
     <div className="App">
+      <Nav isMenuShowing={showMenu} onToggleMenu={handleToggle} />
       <Editor />
     </div>
   );
---
+++ src/Nav/Nav.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import './Nav.css'
+import classNames from "classnames";
+
+function Nav({isMenuShowing, onToggleMenu}) {
+  function ToggleIcon() {
+    if (isMenuShowing) {
+      return (
+        <i className="menu-toggle__icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M2.101.686 23.314 21.9 21.9 23.314.687 2.1z"/>
+            <path d="M.687 21.899 21.9.686 23.314 2.1 2.1 23.313z"/>
+          </svg>
+        </i>
+      )
+    }
+
+    return (
+      <i className="menu-toggle__icon">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 18">
+          <path d="M0 0h30v2H0zM0 8h30v2H0zM0 16h30v2H0z"/>
+        </svg>
+      </i>
+    )
+  }
+
+  useEffect(() => {
+    console.log(isMenuShowing);
+  }, [isMenuShowing])
+  
+  return(
+    <nav className="app-nav">
+      <button className={classNames("menu-toggle", {
+        "menu-toggle--open": isMenuShowing,
+        })}
+        onClick={() => onToggleMenu(!isMenuShowing)}
+      >
+        <ToggleIcon />
+        <span className="menu-toggle__text visually-hidden">{ isMenuShowing ? 'Close menu' : 'Show menu' }</span>
+      </button>
+      <div className="nav__logo">Markdown</div>
+      <div className="nav__document">
+        <div className="document__title">
+          <i className="document__icon"></i>
+          <p className="document__label">Document name</p>  
+          <p className="document__name">welcome.md</p>
+        </div>  
+        <div className="document__controls">
+          <button className="document__delete">
+            <span className="button-text button-text--hidden">Delete</span>
+            <i className="button-icon"></i>
+          </button>
+          <button className="document__save">
+            <span className="button-text">Save changes</span>
+            <i className="button-icon"></i>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
